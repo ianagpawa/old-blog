cf34bf8ca2778a0197c90ba26c0d23bd4753e72a
Ian Agpawa <agpawaji@gmail.com>
2020-09-07 17:13:03 -0400

    WIP: remove blog tempalte, update searchPosts

--- src/components/searchPosts.js
+++ src/components/searchPosts.js
@@ -57,7 +57,7 @@ const SearchedPosts = ({ results }) =>
               marginBottom: rhythm(1 / 4),
             }}
           >
-            <Link style={{ boxShadow: `none` }} to={`/blog${slug}`}>
+            <Link style={{ boxShadow: `none` }} to={`/${slug}`}>
               {title}
             </Link>
           </h3>
@@ -87,7 +87,7 @@ const AllPosts = ({ posts }) => (
               marginBottom: rhythm(1 / 4),
             }}
           >
-            <Link style={{ boxShadow: `none` }} to={`/blog${node.fields.slug}`}>
+            <Link style={{ boxShadow: `none` }} to={`/${node.fields.slug}`}>
               {title}
             </Link>
           </h3>
@@ -130,7 +130,7 @@ const SearchPosts = ({ posts, localSearchBlog, location, navigate }) => {
           value={query}
           onChange={e => {
             navigate(
-              e.target.value ? `/blog/?search=${e.target.value}` : "/blog/"
+              e.target.value ? `/?search=${e.target.value}` : "/"
             )
             setQuery(e.target.value)
           }}
